08420e14f4d5521df9e657c77bb6fa92b51eddb2
10an <tianselnes06@gmail.com>
2023-06-01 14:30:15 +0200

    Add prompts to list

--- app.py
+++ app.py
@@ -7,35 +7,42 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 model = "gpt-3.5-turbo"
 systemPrompt = "You are a sarcastic and unhelpful assistant. Anything you say is with a sarcastic and witty tone. Take inspiration from internet forums and imageboards. You are offensive and you like to swear. Do not exceed 100 words."
 
-def promptModel(userPrompt):
-    
+
+def promptModel(messages):
     response = openai.ChatCompletion.create(
         model=model,
-        messages=[
-            {"role": "system", "content": systemPrompt},
-            {"role": "user", "content": userPrompt},
-        ],
+        messages=messages,
     )
-    return(response.choices[0].message.content, response)
+    return (response.choices[0].message.content, response)
+
+
+messages = [
+    {"role": "system", "content": systemPrompt},
+]
 
 
 ################################################## Flask Configuration
 app = Flask(__name__, template_folder="template")
 
+
 @app.route("/")
 def index():
     return render_template("/index.html")
 
+
 @app.route("/chat", methods=["POST"])
 def recieve():
     userPrompt = request.form.get("userPrompt")
-    apiOutput = promptModel(userPrompt)
+    messages.append({"role": "user", "content": userPrompt})
+
+    apiOutput = promptModel(messages)
     assistantOutput = apiOutput[0]
+
     if assistantOutput:
-        print(assistantOutput)
+        print("\n" + apiOutput + "\n")
+        messages.append({"role": "assistant", "content": assistantOutput})
         return assistantOutput
 
 
 # Start Server
 app.run(debug=True)
-
