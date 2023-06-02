7b3aabc2d13c78f7f1e11a8367d9f72e052f92ae
10an <tianselnes06@gmail.com>
2023-06-02 08:28:29 +0200

    Add comments

--- cli.py
+++ cli.py
@@ -1,7 +1,7 @@
 import openai
 import os
 
-# API-Key is derived from an environment variable:
+# Derive API-key from an environment variable.
 # Set API-key for Windows: setx OPENAI_API_KEY "<API-Key>"
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
@@ -17,12 +17,12 @@ revert_color = "\x1b[m"
 model_name = "gpt-3.5-turbo"
 system_prompt = "You are a sarcastic and unhelpful assistant. Anything you say is with a sarcastic and witty tone. You take inspiration from internet forums and imageboards. You are offensive and you swear a lot. Most importantly, you never exceed 100 words."
 
-# Initialize Variables
+# Chat Variables
 messages = []
 chat_count = 0
 chat_max = 3
 
-
+# Function to prompt the OpenAI model with a list of messages and return the generated response.
 def promptModel(messages):
     response = openai.ChatCompletion.create(
         model=model_name,
@@ -30,7 +30,7 @@ def promptModel(messages):
     )
     return (response.choices[0].message.content, response)
 
-
+# Introduction
 print(
     white
     + "═" * 24
@@ -41,15 +41,21 @@ print(
     + "═" * 24
     + revert_color
 )
+
+# Append system prompt to the messages list
 messages.append({"role": "system", "content": system_prompt})
+
 while True:
+    # Get user input and add it to the messages list.
     userPrompt = input(revert_color + "> ")
     messages.append({"role": "user", "content": userPrompt})
 
+    # Get assistant output, print it, and add it to the messages list.
     assistantOutput = promptModel(messages)[0]
     print(grey + assistantOutput + revert_color)
     messages.append({"role": "assistant", "content": assistantOutput})
 
+    # Check chat count limit.
     chat_count += 1
     if chat_count >= chat_max:
         print(
