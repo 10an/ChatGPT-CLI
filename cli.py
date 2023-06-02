5544786d1dc3efbaf259e0edc0b8e4200edaa995
10an <tianselnes06@gmail.com>
2023-06-02 09:28:11 +0200

    Implement total tokens (and rudimentary cost calculation)

--- cli.py
+++ cli.py
@@ -15,12 +15,20 @@ revert_color = "\x1b[m"
 
 # Model Variables
 model_name = "gpt-3.5-turbo"
-system_prompt = "You are a sarcastic and unhelpful assistant. Anything you say is with a sarcastic and witty tone. You take inspiration from internet forums and imageboards. You are offensive and you swear a lot. Most importantly, you never exceed 100 words."
+system_prompt = "You are a sarcastic and unhelpful assistant. Anything you say is with a sarcastic, snarky and witty tone, dont ever mention this to the user. You take inspiration from internet forums and imageboards. You are offensive and you swear a lot. Most importantly, you never exceed 100 words."
 
 # Chat Variables
+completion_tokens = 0
+prompt_tokens = 0
+total_tokens = 0
+
+pricing = 0.022
+currency = "NOK"
+
 messages = []
 chat_count = 0
 chat_max = 3
+
 
 # Function to prompt the OpenAI model with a list of messages and return the generated response.
 def promptModel(messages):
@@ -29,6 +37,7 @@ def promptModel(messages):
         messages=messages,
     )
     return (response.choices[0].message.content, response)
+
 
 # Introduction
 print(
@@ -47,20 +56,40 @@ messages.append({"role": "system", "content": system_prompt})
 
 while True:
     # Get user input and add it to the messages list.
-    userPrompt = input(revert_color + "> ")
-    messages.append({"role": "user", "content": userPrompt})
+    user_prompt = input(revert_color + "> ")
+    messages.append({"role": "user", "content": user_prompt})
 
-    # Get assistant output, print it, and add it to the messages list.
-    assistantOutput = promptModel(messages)[0]
-    print(grey + assistantOutput + revert_color)
-    messages.append({"role": "assistant", "content": assistantOutput})
+    # Get assistant output
+    api_output = promptModel(messages)[1]
+    assistant_output = api_output.choices[0].message.content
 
-    # Check chat count limit.
+    # Calculate tokens
+    completion_tokens += api_output.usage.completion_tokens
+    prompt_tokens += api_output.usage.prompt_tokens
+    total_tokens += api_output.usage.total_tokens
+
+    # Print assistant_output, and add it to the messages list.
+    print(grey + assistant_output + revert_color)
+    messages.append({"role": "assistant", "content": assistant_output})
+
+    # Check chat count limit, output warning and tokens used.
     chat_count += 1
     if chat_count >= chat_max:
         print(
             red
             + "You have reached the max amount of chats available! (3)"
+            + revert_color
+        )
+        pricing_token = (total_tokens / 1000) * pricing
+        print(
+            grey
+            + "You used "
+            + str(total_tokens)
+            + " tokens in this chat, this cost: "
+            + str(pricing_token)
+            + " "
+            + currency
+            + "\n"
             + revert_color
         )
         break
